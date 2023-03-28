2d542db0c7a6416b71f7042d02761f6f2f1ba826
jhk-im <kjhun200@gmail.com>
2023-03-28 22:46:26 +0900

    feat: working with ui controls

--- SwfitUI/Landmarks/Landmarks/Views/Categories/CategoryHome.swift
+++ SwfitUI/Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -11,10 +11,12 @@
  - 앱 내에서 계층적인 탐색구조 구축
  - NavigationLink 인스턴스와 관련된 modifiers와 함께 사용
 */
+
 import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
+    @State private var showingProfile = false
     
     var body: some View {
         NavigationView {
@@ -31,7 +33,19 @@ struct CategoryHome: View {
                 }
                 .listRowInsets(EdgeInsets())
             }
+            .listStyle(.inset)
             .navigationTitle("Featured")
+            .toolbar {
+                Button {
+                    showingProfile.toggle()
+                } label: {
+                    Label("User Profile", systemImage: "person.crop.circle")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environmentObject(modelData)
+            }
         }
     }
 }
