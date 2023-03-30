7b2596b851a1ca323ffacd6801cd997851f3ce24
jhk-im <kjhun200@gmail.com>
2023-03-30 14:47:20 +0900

    feat: interfacing with uikit

--- SwfitUI/Landmarks/Landmarks/Model/Landmark.swift
+++ SwfitUI/Landmarks/Landmarks/Model/Landmark.swift
@@ -75,6 +75,10 @@ struct Landmark: Hashable, Codable {
         Image(imageName)
     }
     
+    var featureImage: Image? {
+        isFeatured ? Image(imageName + "_feature") : nil
+    }
+    
     private var coordinates: Coordinates
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
